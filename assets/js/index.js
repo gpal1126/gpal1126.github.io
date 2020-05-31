9317b10821633b9481ee8e606ff73431f9137e90
gpal1126 <gpal1126@gmail.com>
2020-05-31 22:35:41 +0900

    포트폴리오 수정

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,34 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.classList.add("dark");
+    document.body.classList.add("dark");
 }
+
+let projNameArr = [ 'chinag9', 'ourhopes', 'hangul', 'surveypro', 'ido' ];
+
+$('.proj-img').on({
+    'mouseover': function(){
+        $(this).children('img').addClass('actived');
+        $(this).append('<div class="more">더보기</div>');
+    },
+    'mouseout': function(){
+        $(this).children('img').removeClass('actived');
+        $(this).children('.more').remove();
+    },
+    'click': function(){
+        let id = parseInt($(this).attr('id').split('_')[1]);
+        $('.popup').css({'display': ''});
+        let projName = projNameArr.filter(function(val, i){
+            if( id === (i+1) ){
+                return val;
+            }
+        });
+
+        console.log('projName:::'+projName);
+        let img = '';
+        for(let i=1; i<=4; i++){
+            img += '<img src="./img/'+projName+'/'+i+'.png" />';
+        }
+        $('.popup').append(img);
+        
+    }
+});
+
